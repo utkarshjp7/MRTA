666a5e3f97a5cfb57f0f6e6c5a36557e01f03b5e
Perl Dev 1 <utkarshjp7@gmail.com>
2018-04-10 15:16:49 -0400

    modified result summary sql file

--- catkin_ws/src/mrta/src/result_summary_query.sql
+++ catkin_ws/src/mrta/src/result_summary_query.sql
@@ -1,14 +1,45 @@
 \connect mrta;
 
 SELECT 
-    robots,
-    tasks,  
-    round(CAST(avg(ms1) as NUMERIC), 4) AS MS1_AVG, 
-    round(CAST(avg(ms2) as NUMERIC), 4) AS MS2_AVG, 
-    round(CAST(avg(tt1) as NUMERIC), 4) AS TT1_AVG,
-    round(CAST(avg(tt2) as NUMERIC), 4) AS TT2_AVG,
-    sum(scheduled_tasks1) AS TOTAL_TASKS1, 
-    sum(scheduled_tasks2) AS TOTAL_TASKS2
-FROM results 
-GROUP BY robots, tasks
-ORDER BY robots, tasks;
+	dcop.robots,
+	dcop.tasks,
+	dcop_ms,
+	dcop_collab_ms,
+	dcop_idle,
+	dcop_collab_idle,
+	dcop_tt,
+	dcop_collab_tt,
+	dcop_tasks,
+	dcop_collab_tasks
+
+FROM
+	(SELECT 
+	    robots,
+	    tasks, 
+	    round(cast(min(ms1) as numeric), 4) as dcop_ms,
+	    round(cast(min(ms2) as numeric), 4) as dcop_collab_ms,
+	    round(cast(min(tt1) as numeric), 4) as dcop_tt,
+	    round(cast(min(tt2) as numeric), 4) as dcop_collab_tt,
+	    round(cast(min(it1) as numeric), 4) as dcop_idle,
+	    round(cast(min(it2) as numeric), 4) as dcop_collab_idle,
+	    sum(scheduled_tasks1) as dcop_tasks,
+	    sum(scheduled_tasks2) as dcop_collab_tasks
+	FROM results
+	WHERE comment = '040918_dcop'
+	GROUP BY robots, tasks) dcop
+/*
+JOIN
+	(SELECT 
+	    robots,
+	    tasks, 
+	    round(cast(min(ms1) as numeric), 4) as pia_ms,
+	    round(cast(min(tt1) as numeric), 4) as pia_tt,
+            sum(scheduled_tasks1) as pia_tasks
+	FROM results
+	WHERE comment = '040218_pia'
+	GROUP BY robots, tasks) as pia
+
+ON dcop.robots = pia.robots AND dcop.tasks = pia.tasks
+*/
+order by  robots, tasks;
+
